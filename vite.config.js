9679d90fdba5735f623910c5749111178b6ec933
suhail786100 <90600987+suhail7861@users.noreply.github.com>
2023-09-20 17:25:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite"
-‌ 
 export default defineConfig({
-    build: {
-        chunkSizeWarningLimit: 100000000
+  base: "/Stakepool-Frontend/",
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "node_modules"),
+      "@": path.resolve(__dirname, "src"),
     },
-})
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+});
